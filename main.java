89711986a19f88bf90aa98533c08d20f8a74028b
badvelsafaansare-lang <badvelsafaansare@gmail.com>
2026-09-07 20:17:30 +0530

    Now user input givingmain.java

--- main.java
+++ main.java
@@ -1,6 +1,6 @@
 import java.util.ArrayList;
 public class Main{
-  public staic void main(String[] args)
+  public static void main(String[] args)
   {
     String name = "safa";
     ArrayList<Integer>marks = new ArrayList<>();
@@ -11,26 +11,64 @@ public class Main{
     System.out.println("student name is:" + name);
     System.out.println("Marks:" + marks);
     int sum = 0;
-    for(int marks : marks){
-      sum = sum+marks;
+    for(int mark : marks){
+      sum = sum+mark;
     }
-    double average = (doible)sum/marks.size();
+    double average = (double)sum/marks.size();
     System.out.println("Average marks: " + average);
-    int highest = marks,get(0);
-    int lowest = marks,get(0);
+    int highest = marks.get(0);
+    int lowest = marks.get(0);
 
-    for(int marks : marks)
+    for(int mark : marks)
       {
-        if (marks > highest)
+        if (mark > highest)
         {
-          highest marks = marks;
+          highest = mark;
         }
-        if(marks < lowest)
+        if(mark < lowest)
         {
-          lowest marks = marks;
+          lowest = mark;
         } 
   }
     System.out.println("Highest marks:" + highest);
-    System.out.println("Lowest marks:" + lowest)'
-}  
+    System.out.println("Lowest marks:" + lowest);
+    boolean passed = true;
+    for(int mark:marks)
+      {
+        if(mark<40)
+        {
+          passed = false;
+          break;
+        }
+      }
+    if(passed)
+    {
+      System.out.println("Result:pass");
+    }
+    else{
+      System.out.println("Result :Fail");
+    }
+    char grade;
+    if(average>=90)
+    {
+      grade='A';
+    }
+    else if(average>=80)
+    {
+      grade='B';
+    }
+    else if(average>=70)
+    {
+      grade='C';
+    }
+    else if(average>=60)
+    {
+      grade='D';
+    }
+    else{
+      grade = 'F';
+    }
+    System.out.println("Grade:" + grade);
+ } 
+}
       
