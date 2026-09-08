90fda62140b5a8d6751c18848b2356707579aa70
badvelsafaansare-lang <badvelsafaansare@gmail.com>
2026-09-08 20:29:30 +0530

    user input complited.java

--- main.java
+++ main.java
@@ -1,25 +1,43 @@
+import java.util.Scanner;
 import java.util.ArrayList;
-public class Main{
+public class Main
+{
   public static void main(String[] args)
   {
-    String name = "safa";
-    ArrayList<Integer>marks = new ArrayList<>();
-    marks.add(85);
-    marks.add(72);
-    marks.add(90);
-    System.out.println("student grade tracker");
-    System.out.println("student name is:" + name);
-    System.out.println("Marks:" + marks);
-    int sum = 0;
-    for(int mark : marks){
-      sum = sum+mark;
-    }
-    double average = (double)sum/marks.size();
-    System.out.println("Average marks: " + average);
-    int highest = marks.get(0);
-    int lowest = marks.get(0);
+    Scanner sc = new Scanner(System.in);
+    System.out.print("Enter number of students:");
+    int students = sc.nextInt();
+    sc.nextLine();
+    for(int s = 1;s<=students;s++)
+    {
+     System.out.println("\nStudent" + s);
+     System.out.println("Enter student name:");
+     String name = sc.next();
+     ArrayList<Integer>marks = new ArrayList<>();
+     System.out.println("Enter number of subjets:");
+     int n = sc.nextInt();
+     for(int i = 1;i<=n;i++)
+      {
+        System.out.print("Enter marks for subject" + i + ":");
+        int mark = sc.nextInt();
+        while(mark<0||mark>100)
+        {
+          System.out.println("Invald marks! Enter marks between 0 and 100.");
+          System.out.print("Enter marks again:");
+          mark = sc.nextInt();
+        }
+         marks.add(mark);
+      }
+       int sum = 0;
+       for(int mark : marks)
+       {
+        sum = sum+mark;
+       }
+        double average = (double)sum/marks.size();
+        int highest = marks.get(0);
+        int lowest = marks.get(0);
 
-    for(int mark : marks)
+       for(int mark : marks)
       {
         if (mark > highest)
         {
@@ -29,11 +47,9 @@ public class Main{
         {
           lowest = mark;
         } 
-  }
-    System.out.println("Highest marks:" + highest);
-    System.out.println("Lowest marks:" + lowest);
-    boolean passed = true;
-    for(int mark:marks)
+      }
+       boolean passed = true;
+       for(int mark:marks)
       {
         if(mark<40)
         {
@@ -41,34 +57,44 @@ public class Main{
           break;
         }
       }
-    if(passed)
-    {
-      System.out.println("Result:pass");
+       char grade;
+       if(average>=90)
+      {
+        grade='A';
+      }
+        else if(average>=80)
+        {
+          grade='B';
+        }
+        else if(average>=70)
+        {
+          grade='C';
+        }
+        else if(average>=60)
+        {
+          grade='D';
+        }
+         else
+         {
+           grade = 'F';
+         }
+         System.out.println("\n---Student Result---");
+         System.out.println("Student Name:" + name);
+         System.out.println("Marks:" + marks);
+         System.out.println("Average Marks:" + average);
+         System.out.println("Highest Marks:" + highest);
+         System.out.println("Lowest Marks:" + lowest);
+         if(passed)
+         {
+           System.out.println("Result: Pass");
+         }else
+          { 
+            System.out.println("Result: Fail");  
+          }
+          System.out.println("Grade:" + grade);
     }
-    else{
-      System.out.println("Result :Fail");
-    }
-    char grade;
-    if(average>=90)
-    {
-      grade='A';
-    }
-    else if(average>=80)
-    {
-      grade='B';
-    }
-    else if(average>=70)
-    {
-      grade='C';
-    }
-    else if(average>=60)
-    {
-      grade='D';
-    }
-    else{
-      grade = 'F';
-    }
-    System.out.println("Grade:" + grade);
+    sc.close();
  } 
 }
+
       
